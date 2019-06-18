7374054386f565627cd2968c8da539ecbb1c4578
朱成中 <956281507@qq.com>
2019-06-18 16:53:50 +0800

    add condition statement

--- jenkinsz/src/main/java/com/zhu/jenkinsz/App.java
+++ jenkinsz/src/main/java/com/zhu/jenkinsz/App.java
@@ -11,6 +11,12 @@ public class App
 	
     public static void main( String[] args )
     {
+    	boolean b=true;
+    	if(b) {
+    		System.out.println("b is true");
+    	}else {
+    		System.out.println("b is false");
+    	}
     	Random random=new Random();
     	int number=random.nextInt(20);
     	System.out.println("number is "+number);
